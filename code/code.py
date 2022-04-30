957d2b5324852e5f7f6057ef253881da45563109
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-04-30 22:41:14 +0530

    Create code.py

---
+++ code/code.py
@@ -0,0 +1,17 @@
+# AI21BTECH11001
+
+# Total Discs = 90
+Discs = 90
+
+# Discs that bear two digit number = 81
+two_digit = 81
+
+# Discs that bear perfect squares = 9
+perfect_squares = 9
+
+# Discs that bear number divisible by 5
+divisible_5 = 18
+
+print(f"The probability of Discs that bear two digit number is ", two_digit/Discs)
+print(f"The probability of Discs that bear oerfect square number is ", perfect_squares/Discs)
+print(f"The probability of Discs that bear number that are divisible by 5 is ", divisible_5/Discs)
